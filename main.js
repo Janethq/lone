99af6a23413a0a84f420f0da2b0c0fd3c19cbbbd
Jane <tanhuiqing95@hotmail.com>
2024-03-27 20:58:27 +0800

    add card

--- main.js
+++ main.js
@@ -1,66 +1,3 @@
-// /*----- constants -----*/
-// const deck = [
-// "a",
-// "a",
-// "b",
-// "b",
-// "c",
-// "c",
-// "d",
-// "d",
-// "e",
-// "e",
-// "f",
-// "f",
-// "g",
-// "g",
-// "h",
-// "h",
-// "i",
-// "i",
-// "j",
-// "j",
-// "lone",
-// ];
-
-// /*----- state variables -----*/
-// const computerHand = [];
-// const playerHand = [];
-// const discardPile = [];
-// const gameCounter = 0;
-// const winCounter = 0;
-// const GAMESTATE = "START GAME";
-
-// /*----- cached elements  -----*/
-
-// /*----- event listeners -----*/
-
-// /*----- functions -----*/
-
-// // https://www.programiz.com/javascript/examples/shuffle-card for card shuffling and display function
-// // shuffle the cards
-// const shuffleDeck = () => {
-//   for (let i = deck.length - 1; i > 0; i--) {
-//     let j = Math.floor(Math.random() * i);
-//     let temp = deck[i];
-//     deck[i] = deck[j];
-//     deck[j] = temp;
-//   }
-//   return deck.join(", ");
-// };
-
-// // display card results
-// const dealCards = () => {
-//   for (let i = 0; i < 10; i++) {
-//     playerHand.push(deck[i]);
-//     console.log(`Player 1's Hand: ${playerHand.join(", ")}`);
-//   }
-// };
-
-// const main = () => {
-//   shuffleDeck();
-//   dealCards();
-// };
 const cards = [
   "a",
   "a",
@@ -127,25 +64,6 @@ const startGame = () => {
 
 const startButton = document.querySelector("#startButton");
 startButton.addEventListener("click", startGame);
-
-// function checkForMatch(playerHand) {
-//   for (let i = 0; i < playerHand.length; i++) {
-//     if (playerHand[i] === "O") {
-//       let matchIndex = playerHand.findIndex(card => card !== "O" && card.charAt(0) === playerHand[i].charAt(0));
-
-//       if (matchIndex !== -1) {
-//         let matchCard = playerHand[matchIndex];
-//         console.log("Player has a match with cards: " + playerHand[i] + " and " + matchCard);
-
-//         playerHand.splice(i, 1);
-//         playerHand.splice(matchIndex, 1);
-
-//         return true;
-//       }
-//     }
-//   }
-//   return false;
-// }
 
 const checkForMatch = (arr) => {
   for (i = 0; i < arr.length; i++) {
