4e8b4aec664fd71ce5b0ebaf1f658fca02def514
Jane <tanhuiqing95@hotmail.com>
2024-03-28 22:25:31 +0800

    added logic to discard pairs

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let player1Hand = [];
 let player2Hand = [];
 let discardPile = [];
+let theresPairs = false;
 const cards = [
   "card1",
   "card1",
@@ -31,7 +32,7 @@ const dealCards = () => {
   //get first half of deck + 1 extra card
   player1Hand = shuffledCards.slice(0, 6);
   //get second half of deck
-  player2Hand = shuffledCards.slice(7, 11);
+  player2Hand = shuffledCards.slice(6, 11);
   //display it
   document.querySelector("#player1Cards").innerHTML = player1Hand
     .map((card) => `<div class='card'>${card}</div>`)
@@ -41,6 +42,37 @@ const dealCards = () => {
     .join("");
 };
 
-const player2DiscardPairs =() =>{
-  
-}
+const player2DiscardPairs = () => {
+  checkForPairs(player2Hand);
+};
+
+//check if theres pairs
+const checkForPairs = (handArray) => {
+  let pairs = handArray.reduce((acc, curr) => {
+    if (handArray.indexOf(curr) !== handArray.lastIndexOf(curr)) {
+      acc.push(curr);
+    }
+    return acc;
+  }, []);
+
+  console.log(`pairs: ${pairs}`);
+
+  if (pairs.length >= 1) {
+    theresPairs = true;
+    console.log("there are pairs");
+    document.querySelector("#discardedCards").innerHTML = pairs
+      .map((card) => `<div class='card'>${card}</div>`)
+      .join("");
+    //update player2Hand without discarded pairs
+    player2Hand = player2Hand.filter((card) => !pairs.includes(card));
+    console.log(`Updated player 2 hand: ${player2Hand}`);
+    document.querySelector("#player2Cards").innerHTML = player2Hand
+      .map((card) => `<div class='card'>${card}</div>`)
+      .join("");
+  } else {
+    document.querySelector("#discardedCards").innerHTML =
+      "You have no pairs right now";
+    console.log("no pairs");
+  }
+  return pairs;
+};
